cbc8c23c61faf561d91af8424427256c42ef8a61
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-15 20:11:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: "Lord Monague",
+        name: "Lord Montague",
         age: "53"
         },
       :matriarch => {
-        name: "Lady Monatgue",
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
